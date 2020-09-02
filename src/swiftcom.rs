c34fa044bcdc8952ceee6a6cd1593d3a9f7bc5c4
Blaine <rishi1998@gmail.com>
2020-09-02 13:56:07 +0530

    implement utilities

--- src/swiftcom.rs
+++
@@ -1,5 +0,0 @@
-pub struct CountableCloseRange<T> 
-where T: std::cmp::Ord{
-    low: T,
-    high: T,
-}
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,74 @@
-pub fn calculate_pattern_alphabet(pattern: &str) -> Vec<char> {
-    let len = pattern.len;
-    let mask = vec!();
-    for (i, c) in pattern.chars().enumerate()  {
-        mask
+use std::collections::HashMap;
+use std::ops::{Add, Sub, Range};
+
+pub fn calculate_score(
+    pattern: String,
+    e: i32,
+    x: i32,
+    loc: i32,
+    distance: i32) -> f64 {
+        calculate_score_with_length(
+            pattern.len() as u32,
+            e,
+            x,
+            loc,
+            distance,
+        )
+}
+
+pub fn calculate_score_with_length(
+    pattern_length: u32,
+    e: i32,
+    x: i32,
+    loc: i32,
+    distance: i32) -> f64 {
+    let accuracy = (e as f64) / (pattern_length as f64);
+    let proximity = (x - loc).abs();
+    if distance == 0 {
+        return if proximity != 0 { 1. } else { accuracy as f64} 
+    }
+    accuracy + (proximity as f64) / ( distance as f64)
+}
+
+/// Initializes the alphabet for the Bitap algorithm
+///
+/// - Parameter pattern: The text to encode.
+/// - Returns: Hash of character locations.
+pub fn calculate_pattern_alphabet(pattern: &str) -> HashMap<char, u32> {
+    let len = pattern.len();
+    let mask = HashMap::new();
+    for (i, c) in pattern.chars().enumerate() {
+        mask.insert(c, mask.get(&c).unwrap_or(&0) | (1 << (len -i -1)));
     }
     mask
+}
+
+/// Returns an array of `CountableClosedRange<Int>`, where each range represents a consecutive list of `1`s.
+///
+///     let arr = [0, 1, 1, 0, 1, 1, 1 ]
+///     let ranges = findRanges(arr)
+///     // [{startIndex 1, endIndex 2}, {startIndex 4, endIndex 6}
+///
+/// - Parameter mask: A string representing the value to search for.
+///
+/// - Returns: `CountableClosedRange<Int>` array.
+pub fn find_ranges(mask: &[u8]) -> Result<Vec<Range<u32>>, String> {
+    if mask.is_empty() {
+        return Err(String::from("Input array is empty"));
+    }
+    let ranges = vec!();
+    let start: i32 = -1;
+    for (n, bit) in mask.iter().enumerate() {
+        if start == -1 && *bit >= 1{
+            start = n as i32;
+        } else if start != -1 && *bit == 0 {
+            ranges.push(start as u32..n as u32);
+            start = -1;
+        }
+    }
+
+    if *mask.last().unwrap() == 1 {
+        ranges.push(start as u32..mask.len() as u32)
+    }
+    Ok(ranges)
 }
