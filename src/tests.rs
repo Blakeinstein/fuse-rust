a1fcf6671ca1f968b2980b7d0cd71ffbd095bac4
Blaine <rishi1998@gmail.com>
2020-12-23 18:17:45 +0530

    Add test for multibyte chars indices.

--- src/tests.rs
+++ src/tests.rs
@@ -17,3 +17,19 @@ fn multibyte_chars() {
         .search_text_in_iterable(pat, s.iter())
         .is_empty());
 }
+
+#[test]
+fn multibyte_chars_indices() {
+    let needle = "f";
+    let s = "®f∮";
+
+    let fuse = Fuse::default();
+    let pat = fuse.create_pattern(needle);
+    let x = fuse.search(pat.as_ref(), s).unwrap();
+    let r = &x.ranges[0];
+
+    assert_eq!(
+        &s[r.start..r.end],
+        needle
+    );
+}
